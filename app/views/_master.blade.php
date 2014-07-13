e6c7a78bbf9b12a2932e7edae0e4947401fe720a
Heather Baird <hbaird@fas.harvard.edu>
2014-07-13 00:54:37 -0400

    added css

--- app/views/_master.blade.php
+++ app/views/_master.blade.php
@@ -7,7 +7,7 @@
 
 
 	<link href="//netdna.bootstrapcdn.com/bootswatch/3.1.1/flatly/bootstrap.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="styles/foobooks.css" type="text/css">
+	<link rel="stylesheet" href="styles/custom.css" type="text/css">
 
 
 	@yield('head')
